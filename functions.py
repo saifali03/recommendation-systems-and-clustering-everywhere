e38b397f99757982c8c70c51aabcb99d1d395de0
Daniel Stroth <stroth.2117444@studenti.uniroma1.it>
2023-12-11 23:23:46 +0100

    deletes superfluous stuff from functions

--- functions.py
+++ functions.py
@@ -16,83 +16,6 @@ def calculate_distances(center, data, essential_cols):
     distances = data.apply( lambda row : ((center-row[essential_cols])**2).sum(axis=1), axis=1) # calculating centroids w.r.t. first centroid
     return distances
 
-def get_center_by_weighted_random(data, distances):
-    weights = distances.to_numpy().reshape(-1)
-    center = np.random.choice( np.arange(0,len(data)), p=weights/sum(weights) )
-    return pd.DataFrame(data.iloc[center,]).T
-
-
-def initialise_clusters_standard(k, data, essential_cols):
-    centers = data[essential_cols].sample(n=k).copy() # drawing k samples uniformly random without replacement
-
-    # add distances w.r.t to the last created center
-    distances_df = pd.DataFrame()
-    for i in range(k):
-        center = pd.DataFrame(centers.iloc[i].copy()).T
-        distances_df['center_' + str(i)] = calculate_distances(center, data, essential_cols)
-    return distances_df.idxmin(axis=1)
-
-
-
-def initialise_clusters_adv(k, data, essential_cols):
-    distances_df = pd.DataFrame()
-    centers = pd.DataFrame()
-
-    for i in range(k):
-        # create centers
-        if len(centers) == 0:
-            center = data[essential_cols].sample(n=1).copy()
-        else:
-            distances = distances_df.min(axis=1)
-            center = get_center_by_weighted_random(data, distances)
-        centers = pd.concat([centers, center])
-        # add distances w.r.t to the last created center
-        distances_df['center_' + str(i)] = calculate_distances(center, data, essential_cols)
-    return distances_df.idxmin(axis=1) #  centers # data['cluster'] =
-
-
-def calculate_new_centers(data, essential_cols): # this is the "reduce" of map-reduce
-    return data.groupby('cluster')[essential_cols].mean()
-
-def get_new_clusters(data, centers, essential_cols): # this is the "map" of map-reduce
-    k = len(centers)
-    distances_df = pd.DataFrame()
-    for i in range(k):
-        center = pd.DataFrame(centers.iloc[i,:]).T
-        distances_df['center_' + str(i)] = calculate_distances(center, data, essential_cols)
-        error = distances_df.min(axis=1).sum()
-
-    return error, distances_df.idxmin(axis=1)
-
-
-def iterate_clustering(data, essential_cols, eps=0.1, max_iterations=None ):
-    error = eps + 1
-    error_old = 2*error
-    i=0
-    while abs(error-error_old) > eps:
-        if max_iterations is not None:
-            i = i+1
-            if i >= max_iterations:
-                break
-        error_old = error
-        centers = calculate_new_centers(data, essential_cols)
-        error, clusters = get_new_clusters(data, centers, essential_cols)
-        data['cluster'] = clusters
-
-
-def our_k_means_adv_old(k, data, essential_cols, eps=0.1, max_iterations=None):
-    data = data.copy()
-    data['cluster'] = initialise_clusters_adv(k, data, essential_cols)
-    iterate_clustering(data, essential_cols, eps=eps, max_iterations=max_iterations)
-    return data
-
-def our_k_means_standard_old(k, data, essential_cols, eps=0.1, max_iterations=None):
-    data = data.copy()
-    data['cluster'] = initialise_clusters_standard(k, data, essential_cols)
-    iterate_clustering(data, essential_cols, eps=eps,  max_iterations=max_iterations)
-    return data
-
-
 def compute_wcss(k, data, essential_cols):
     """
     Takes a dataframe and the number of clusters you chose and compute the WCSS (the sum
@@ -106,8 +29,9 @@ def compute_wcss(k, data, essential_cols):
         dist = calculate_distances(center, cluster_pts, essential_cols)
         wcss+= dist.sum()
     return wcss
-   
-   
+
+
+# k-means implementations (RQ 2.3.1 and 2.3.4)
    	
 def our_k_means_standard(k, data_path, essential_cols, nrows=10**4, chunksize=10**3, upper_bound_for_iterations=50 ):
     centers = init_centers_standard(k, data_path, essential_cols, nrows=nrows)
